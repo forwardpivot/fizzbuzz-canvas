ba9006894cb49f539bb0795a346d0074da75b6de
Taby <forwardpivot@proton.me>
2022-10-28 15:31:57 -0500

    Trying to make a bee will probably change the stripes to use lines instead of arcs

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+let canvas = document.querySelector('#canvas');
+let ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function bee() {
+    //The body of the bee
+    ctx.fillStyle = 'yellow';
+    ctx.beginPath();
+    ctx.ellipse(200, 200, 35, 25, 25.1, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    //The stripes on the bee
+    ctx.fillStyle = 'black';
+    ctx.beginPath();
+    ctx.arc(185, 214, 41, 5, .01);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(165, 215, 43, 5, .01);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+}
+bee();
+
+//The Fizzbuzz logic
 for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
         console.log('FizzBuzz');
